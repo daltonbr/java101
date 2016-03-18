ba448dc4b180f859810e9f0081ded11234e5f22f
daltonbr <sacinopatinete@gmail.com>
2016-03-18 18:59:16 -0300

    [wip] Exercicios aula02

---
+++ country/src/com/dalton/country/Country.java
@@ -0,0 +1,106 @@
+package com.dalton.country;
+import java.util.ArrayList;
+//import com.dalton.country.Country;
+
+/**
+ * Created by dalton on 3/15/16.
+ *
+ * 3) Escreva uma classe em Java que represente um país. Um país tem como atributos o
+ seu nome, o nome da capital, sua dimensão em Km2 e uma lista de países com os quais
+ ele faz fronteira.  Forneça os seguintes construtores e método:
+
+ a) Construtor que inicialize o nome, capital e a dimensão do país;
+
+ b) Métodos de acesso (getters/setters) para as propriedades indicadas no item (a);
+
+ c) Um método que permita verificar se dois países são iguais. Dois países são iguais
+ se tiverem o mesmo nome e a mesma capital. A assinatura deste método deve ser:
+
+ public boolean equals(Pais outro);
+
+ d) Um método que define quais outros países fazem fronteira (note que um país não
+ pode fazer fronteira com ele mesmo);
+
+ e) Um método que retorne a lista de países que fazem fronteira;
+
+ f) Um método que receba um outro país como parâmetro e retorne uma lista de
+ vizinhos comuns aos dois países.
+ */
+
+public class Country {
+    private String name;
+    private String capital;
+    private int area;  // country's area in Km2
+    // list of boarders countries - array list
+
+    ArrayList<Country> borders = new ArrayList();
+
+    //constructor
+    public Country (String _name, String _capital, int _area) {
+        this.setName(_name);
+        this.setCapital(_capital);
+        this.setArea(_area);
+    }
+
+    //setters and getters
+    public void setName(String _name) {
+        this.name = _name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setCapital(String _capital) {
+        this.capital = _capital;
+    }
+
+    public String getCapital() {
+        return this.capital;
+    }
+
+    public void setArea(int _area) {
+        this.area = _area;
+    }
+
+    public int getArea() {
+        return this.area;
+    }
+
+    public String getBorders () {
+        StringBuilder output = new StringBuilder();
+
+        this.borders.forEach((temp) -> {
+            output.append(temp.getName());
+        });
+
+        return output.toString();
+    }
+
+    public boolean equals(Country _outro) {
+        boolean result;
+        result = ((this.getName() == _outro.getName()) && (this.getCapital() == _outro.getCapital())) ?
+                true : false;
+
+        return result;
+    }
+
+    public ArrayList<Country> commonBorders(Country _other) {
+        ArrayList<Country> output = new ArrayList();
+
+        for (Country temp : this.borders ) {
+            if ( _other.borders.contains(temp) ) output.add(temp);
+        }
+        return output;
+    }
+
+    public String arrayListToString() {
+        StringBuilder output = new StringBuilder();
+
+        for (Country temp : this.borders ) {
+            output.append(temp.getName());
+        }
+        return output.toString();
+    }
+
+}
---
+++ country/src/com/dalton/country/Main.java
@@ -0,0 +1,94 @@
+package com.dalton.country;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by dalton on 3/15/16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Country bra = new Country("Brazil", "Brasilia", 8514877);
+        Country bra2 = new Country("Brazil", "Brasilia", 500);
+        Country bra3 = new Country("Brazil", "HUE HUE", 1000);
+
+        Country arg = new Country("Argentina", "Buenos Aires", 2766890);
+        Country bol = new Country("Bolivia", "Bogota", 1098580);
+        Country chi = new Country("Chile", "Santiago", 756950);
+        Country col = new Country("Colombia", "Bogota", 1141748);
+        Country ecu = new Country("Ecuador", "Quito", 283560);
+        Country fal = new Country("Falkland Islands", "Stanley", 12173);
+        Country frg = new Country("French Guiana", "Cayenne", 91000);
+        Country guy = new Country("Guyana", "Georgetown", 214999);
+        Country par = new Country("Paraguay", "Asuncion", 406752);
+        Country per = new Country("Peru", "Lima", 1285220);
+        Country sge = new Country("South Georgia", "King Edward Point", 3093);
+        Country sur = new Country("Suriname", "Paramaribo", 163270);
+        Country uru = new Country("Uruguay", "Montevideo", 176215);
+        Country ven = new Country("Venezuela", "Caracas", 916445);
+
+        System.out.println(bra.getName() + bra.getCapital() + bra.getArea());
+        System.out.println (bra.equals(bra2) );   // br == br2  (true)
+        System.out.println (bra.equals(bra3) );   // BR != br3  (false)
+
+        //Brazil borders with: Suriname, Guyana, Venezuela, Colombia, Peru, Bolivia, Paraguay, Argentina, Uruguay
+        bra.borders.addAll(Arrays.asList(sur, guy, ven, col, per, bol, par, arg, uru) );
+
+        //Argentina borders with: Chile, Paraguay, Brazil, Bolivia, Uruguay
+        arg.borders.addAll(Arrays.asList(chi, par, bra, bol, uru) );
+
+        //Bolivia Borders with: Brazil, Paraguay, Argentina, Chile, Peru;
+        bol.borders.addAll(Arrays.asList(bra, par, arg, chi, per ) );
+
+        //Chile borders with: Peru, Bolivia, Argentina
+        chi.borders.addAll(Arrays.asList(per, bol, arg));
+
+        //Colombia borders with: Venezuela, Brazil, Ecuador, Peru
+        col.borders.addAll(Arrays.asList(ven, bra, ecu, per));
+
+        //Ecuador borders with: Colombia, Peru
+        ecu.borders.addAll(Arrays.asList(col, per));
+
+        //Falkland Islands borders with: NONE - it's an island =)
+        fal.borders = null;
+
+        //French Guiana borders with: Brazil, Suriname
+        frg.borders.addAll(Arrays.asList(bra, sur));
+
+        //Guyana borders with: Brazil, Venezuela
+        guy.borders.addAll(Arrays.asList(bra, ven));
+
+        //Paraguay borders with: Argentina, Brazil, Bolivia
+        par.borders.addAll(Arrays.asList(arg, bra, bol) );
+
+        //Peru borders with: Ecuador, Colombia, Brazil, Bolivia, Chile
+        per.borders.addAll(Arrays.asList(ecu, col, bra, bol, chi));
+
+        //South Georgia borders with NONE - another Island
+        sge.borders = null;
+
+        //Suriname borders with: French Guiana, Guyana, Brazil
+        sur.borders.addAll(Arrays.asList(frg, guy, bra));
+
+        //Uruguay borders with: Argentina, Brazil
+        uru.borders.addAll(Arrays.asList(arg, bra) );
+
+        //Venezuela  borders with: Colombia, Brazil, Guyana
+        ven.borders.addAll(Arrays.asList(col, bra, guy));
+
+        // Displaying borders
+        System.out.println(bra.getName() + " borders with: " + bra.getBorders() );
+        System.out.println(arg.getName() + " borders with: " + arg.getBorders() );
+        System.out.println(bol.getName() + " borders with: " + bol.getBorders() );
+//        System.out.println(( printArrayList ( bra.commonBorders(arg) ) ) );
+        System.out.println(printArrayList(bra.commonBorders(arg)) );
+
+
+
+
+//        System.out.println(chi.getName() + " borders with: " + chi.borders);
+//        System.out.println(col.getName() + " borders with: " + col.borders);
+//        System.out.println(ecu.getName() + " borders with: " + ecu.borders);
+//        System.out.println(fal.getName() + " borders with: " + fal.borders);
+    }
+}
