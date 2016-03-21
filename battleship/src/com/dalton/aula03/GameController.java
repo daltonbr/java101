6c0cff4eab676c5694e6afd5e097c56d1f88373b
daltonbr <sacinopatinete@gmail.com>
2016-03-21 15:33:12 -0300

    BattleShip v0.2.7 - drawBoard now showing labels

--- battleship/src/com/dalton/aula03/GameController.java
+++ battleship/src/com/dalton/aula03/GameController.java
@@ -9,12 +9,12 @@ import java.util.ArrayList;
 public class GameController {
     final int BOARD_WIDTH = 5;  //hardcoded for now
     final int BOARD_HEIGHT = 5;
-    ArrayList<Cell> _cells = new ArrayList<>(); // reference to cells in the board
+    ArrayList<Cell> _cellsArray = new ArrayList<>(); // reference to cells in the board
 
     public void startGame()
     {
         Grid newBoard = new Grid(BOARD_WIDTH,BOARD_HEIGHT);   // instantiate a new board
-       _cells = newBoard.getBoard();     // get a reference to the board (inside grid object)
+       _cellsArray = newBoard.getBoard();     // get a reference to the board (inside grid object)
 
         this.drawBoard();  // draw a board to the screen
 
@@ -24,15 +24,23 @@ public class GameController {
     {
         System.out.println("Drawing Board: ");
         int _line = BOARD_HEIGHT - 1;
+        System.out.print(_line);  // printing the label
 
-        for (Cell temp : _cells )   // print all the cells in the arraylist
+        for (Cell temp : _cellsArray)   // print all the cells in the arraylist
         {
             if ( _line != temp.getX() )  // detect when we change lines, and jump a line
             {
                 System.out.println("");
+                System.out.print(_line - 1);  // printing the label
             }
-            System.out.print("x: " + temp.getX() + " y: " + temp.getY() + " - ");
+            System.out.print(" - " + temp.getX() + temp.getY());
             _line = temp.getX();
+        }
+        System.out.println();
+        System.out.print(" ");
+        for (int i = 0; i < BOARD_WIDTH ; i++ )
+        {
+            System.out.print(" -  " + i);
         }
     }
 
